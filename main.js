453bb2316813f224102b92447bdcc70c073c24bf
Richard <ricky201325@gmail.com>
2024-11-25 01:39:18 -0300

    primer commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const web = require('./module/myModule.js');
+const {myArray,myNumber,myWebAddress,user} = require("./module/myModule.js");
+
+console.log(web)
+console.log(myArray)
+
+console.log(myNumber)
+
+console.log(myWebAddress)
+
+console.log(user)
---
+++ module/myModule.js
@@ -0,0 +1,16 @@
+const myWebAddress = "richard.com"
+module.exports = myWebAddress
+console.log(module)
+
+const myNumber = 30
+const myArray = [10,20,30]
+const user = {
+    name:'ryan',
+    lastname: "ray"
+}
+module.exports = {
+    myWebAddress,
+    myNumber,
+    myArray,
+    user
+}
