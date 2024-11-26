2d23f2b83b4ac5bf5d50c2fee33f381647842778
Richard <ricky201325@gmail.com>
2024-11-26 00:10:37 -0300

    servidor basico usando el objeto de request, ya que nos recibe la url

--- http.js
+++ http.js
@@ -1,8 +1,28 @@
 const http = require('http');
 
-http.createServer(function (req, res) {
-    res.write('Hello world'); // Corregido: Cambiado de req.write a res.write
-    res.end();
-}).listen(3000);
+const server = http.createServer(function (req, res) {
+    console.log(req.url); // Corregido: Cambiado de request.url a req.url
+
+    if (req.url === '/') { // Corregido: Cambiado de request.url a req.url
+        res.write('Bienvenido'); // Corregido: Cambiado de response.write a res.write
+        return res.end(); 
+    }
+
+    if (req.url === '/about') { // Corregido: Cambiado de request.url a req.url
+        res.write('Acerca de'); // Corregido: Cambiado de response.write a res.write
+        return res.end(); // Corregido: Cambiado de response.end a res.end
+    }
+
+    res.write(`
+        <h1>Not Found</h1>
+        <p>esta pagina no se encontro</p>
+        <a href="/">volver a la pagina principal</a>`); 
+    res.end(); 
+})
+
+
+server.listen(3000);
+
+
 
 console.log("Servidor escuchando el puerto 3000");
